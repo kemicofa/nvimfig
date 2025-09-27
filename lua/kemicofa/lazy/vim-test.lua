b7bb2f8712ed54a3a105c701e3e99ea2b6ba1e87
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2025-09-27 18:36:51 +0200

    added config for vim-test

---
+++ lua/kemicofa/lazy/vim-test.lua
@@ -0,0 +1,12 @@
+return {
+    "vim-test/vim-test",
+
+    config = function()
+        vim.keymap.set("n", "<leader>t", ":TestNearest<CR>")
+        vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
+        vim.keymap.set("n", "<leader>a", ":TestSuite<CR>")
+        vim.keymap.set("n", "<leader>l", ":TestSuite<CR>")
+
+        vim.g["test#javascript#jest#executable"] = "npm test --"
+    end
+}
