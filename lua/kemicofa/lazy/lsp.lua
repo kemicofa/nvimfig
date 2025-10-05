cfa297ddbce7f5a6d6916d409aa23bfee36b3d1b
kemicofa <kevin.faulhaber@gmail.com>
2025-10-05 11:36:13 +0200

    prettier

--- lua/kemicofa/lazy/lsp.lua
+++ lua/kemicofa/lazy/lsp.lua
@@ -26,10 +26,6 @@ return {
     },
 
     config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-            }
-        })
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
@@ -44,7 +40,6 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-                "gopls",
                 "tailwindcss",
             },
             handlers = {
